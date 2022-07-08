a4da9dcf0d27ee05fe395a13f585e2faa641973f
NikDoe <nikd0e@yandex.ru>
2022-07-08 16:15:45 +0300

    feature(maze): pick a random cell

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { Engine, Render, Runner, World, Bodies } = Matter;
 
-const cells = 5;
+const cells = 3;
 const width = 600;
 const height = 600;
 
@@ -40,4 +40,20 @@ const horizontalsWalls = Array(cells - 1)
 	.fill(null)
 	.map(() => Array(cells).fill(false));
 
-console.log(grid, verticalsWalls, horizontalsWalls);
+const startRow = Math.floor(Math.random() * cells);
+const startColumn = Math.floor(Math.random() * cells);
+
+console.log(startRow, startColumn);
+
+const stepThroughCell = (row, column) => {
+	// If i have visited the cell at [row, column], then return
+	// Mark this cell as being visited
+	// Assemble randomly-ordered list of neighbors
+	// For each neighbor....
+	// See if that neighbor is out of bounds
+	// If we have visited that neighbor, continue to next neighbor
+	// Remove a wall from either horizontals or verticals
+	// Visit that next cell
+};
+
+stepThroughCell(startRow, startColumn);
